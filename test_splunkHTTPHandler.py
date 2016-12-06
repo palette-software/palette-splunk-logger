df547c0bde962a7020a93ba729bd8def505e0934
SELLER Peter Andras <spa@palette-software.com>
2016-12-06 17:17:12 +0100

    Use LogRecord in tests

--- test_splunkHTTPHandler.py
+++ test_splunkHTTPHandler.py
@@ -1,25 +1,34 @@
 from unittest import TestCase
 from unittest.mock import patch
 
+import logging
+
 from splunkhttphandler import SplunkHTTPHandler
-
-
-class Record(object):
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-
 
 class TestSplunkHTTPHandler(TestCase):
     def setUp(self):
         self.splunk_http_handler = SplunkHTTPHandler('host', 'url', 'token')
-        self.logrecord = Record(
-            **{'relativeCreated': 6.676912307739258, 'funcName': '<module>', 'lineno': 15, 'name': 'simple_example',
-               'levelname': 'DEBUG', 'filename': 'test_logger.py', 'msecs': 678.5240173339844, 'exc_info': None,
-               'created': 1480955547.678524, 'stack_info': None, 'process': 10581, 'exc_text': None, 'args': (),
-               'levelno': 10,
-               'thread': 140736880042944, 'processName': 'MainProcess', 'msg': 'debug message',
-               'pathname': '/Users/spa/Git/palette-splunk-logger/test_logger.py', 'threadName': 'MainThread',
-               'module': 'test_logger'})
+        self.logrecord = logging.LogRecord('simple_example',
+                                           10,
+                                           '/Users/spa/Git/palette-splunk-logger/test_logger.py',
+                                           15,
+                                           'debug message',
+                                           None,
+                                           None,
+                                           '<module>',
+                                           None
+                                           )
+        self.logrecord.created = 1480955547.678524
+        self.logrecord.msecs = 678.5240173339844
+        self.logrecord.relativeCreated = 6.676912307739258
+
+        #     **{'relativeCreated': 6.676912307739258, 'funcName': '<module>', 'lineno': 15, 'name': 'simple_example',
+        #        'levelname': 'DEBUG', 'filename': 'test_logger.py', 'msecs': 678.5240173339844, 'exc_info': None,
+        #        'created': 1480955547.678524, 'stack_info': None, 'process': 10581, 'exc_text': None, 'args': (),
+        #        'levelno': 10,
+        #        'thread': 140736880042944, 'processName': 'MainProcess', 'msg': 'debug message',
+        #        'pathname': '/Users/spa/Git/palette-splunk-logger/test_logger.py', 'threadName': 'MainThread',
+        #        'module': 'test_logger'})
         self.splunk_event = self.splunk_http_handler.mapLogRecord(self.logrecord)
 
     def test_event_has_no_time(self):
@@ -38,3 +47,15 @@ class TestSplunkHTTPHandler(TestCase):
         logevent = self.splunk_event["event"]
         self.assertEqual('debug message', logevent["message"])
         self.assertEqual("DEBUG", logevent["level"])
+
+    def test_formatted_output_without_formatter(self):
+        splunk_event = self.splunk_http_handler.mapLogRecordWithFormat(self.logrecord)
+
+        self.assertEqual('debug message', splunk_event["event"])
+
+    def test_formatted_output_with_formatter(self):
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.splunk_http_handler.setFormatter(formatter)
+        splunk_event = self.splunk_http_handler.mapLogRecordWithFormat(self.logrecord)
+
+        self.assertEqual('2016-12-05 17:32:27,678 - simple_example - DEBUG - debug message', splunk_event["event"])
