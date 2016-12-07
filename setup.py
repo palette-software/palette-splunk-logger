33781415e0b4b343c1efcd4726c4072c159e67a0
SELLER Peter Andras <spa@palette-software.com>
2016-12-07 11:14:13 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='palette-splunk-logger',
+
+    version='1.0.0',
+    license='Apache License 2.0',
+    platforms='Linux',
+
+    description=('The SplunkHTTPHandler class is  a subclass of the logging.Handler'
+                 ', supports sending logging messages to a Splunk server via it HTTP Event Collector'),
+
+    author='Palette Developers',
+    author_email='developers@palette-software.com',
+    url='https://github.com/palette-software/palette-splunk-logger/',
+
+    packages=find_packages(),
+    include_package_data=True
+)
